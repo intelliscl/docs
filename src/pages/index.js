070c129baa9aa9133e481acd8469997e98520724
Dave Philp <dphilp@rtg.com.au>
2021-02-13 17:47:54 +1100

    Minor tidy up

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,8 +71,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Intellischool Docs"
+      description="Find all the technical documentation you need on Intellischool's APIs, data platform, and integrated services.">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
